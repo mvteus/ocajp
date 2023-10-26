37e3d03acf209232aa70754ecc5199e714cec1b9
Mateus Souza da Silva <mma.teuzzz@gmail.com>
2023-10-25 21:43:48 -0300

    initial commit

---
+++ exerc02.java
@@ -0,0 +1,8 @@
+class TExerc02 {
+    public static void main(String[] args){
+        for(int i=0; i<20;i++)
+            System.out.println(i);
+            System.out.println(i);
+        System.out.println("finished");
+    }
+}
---
+++ exerc05.java
@@ -0,0 +1,9 @@
+class Exerc05 {
+    static int i = 3;
+
+    public static void main(String[] args) {
+        for (new Exerc05().i = 10; new Exerc05().i < 100; new Exerc05().i++) {
+            System.out.println(i);
+        }
+    }
+}
